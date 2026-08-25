7d7dcf02e30425818aabde99b56405204e67bf8e
guishe2207 <silvapinedaguillermoandres@gmail.com>
2026-08-25 15:53:27 -0500

    otro arreglo peldon

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 function conectarDB() {
-  return mongoose.connect('mongodb+srv://gshsilva2207_db_user:<Guishe2207>@cluster0.cuhrrz2.mongodb.net/quiz');
+  return mongoose.connect( 'mongodb+srv://gshsilva2207_db_user:Guishe2207@cluster0.cuhrrz2.mongodb.net/quiz');
 }
 
 module.exports = conectarDB;
